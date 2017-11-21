ba828ead6116f262c58f21933c67e78e4f67b806
Simon Sturmer <sstur@me.com>
2017-11-21 14:15:49 +0700

    Move ContactList to its own scene

---
+++ src/ContactListScene.js
@@ -0,0 +1,51 @@
+// @flow
+import React, {Component} from 'react';
+import {View, Text, Alert, Platform} from 'react-native';
+import Touchable from './Touchable';
+import Button from './Button';
+
+import styles from './ContactListScene-styles';
+
+let contactList = [
+  {name: 'Ramzi', phoneNumber: '08123456789'},
+  {name: 'Indra', phoneNumber: '085551212098'},
+];
+
+function Contact(props) {
+  let {contact, showPhoto} = props;
+  let profilePhoto =
+    showPhoto === true ? (
+      <View style={styles.profilePhoto}>
+        <Text style={styles.letter}>{contact.name.charAt(0)}</Text>
+      </View>
+    ) : null;
+  let onPress = () => {
+    Alert.alert('Contact Pressed', `Phone number: ${contact.phoneNumber}`, [
+      {text: 'Dismiss'},
+    ]);
+  };
+  return (
+    <Touchable onPress={onPress}>
+      <View style={styles.contact}>
+        {profilePhoto}
+        <Text style={styles.contactName}>{contact.name}</Text>
+      </View>
+    </Touchable>
+  );
+}
+
+function ContactListScene() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>Contact List</Text>
+      </View>
+      {contactList.map(contact => (
+        <Contact key={contact.name} contact={contact} showPhoto={true} />
+      ))}
+      <Button label="Add New" onPress={() => console.log('add new')} />
+    </View>
+  );
+}
+
+export default ContactListScene;
